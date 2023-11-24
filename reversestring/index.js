bfeae6986ca32781a4db424e34dfa730a3171309
Rishabh Chauhan <rishabhchauhan@Rishabhs-MacBook-Air.local>
2023-11-24 09:11:02 +0530

    Added reverseString solution

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Solution 1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+//Solution 2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (var i = 0; i < str.length; i++) {
+//     character = str.charAt(i);
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+//Solution 3
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
 
 module.exports = reverse;
